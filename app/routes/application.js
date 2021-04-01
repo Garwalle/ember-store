5e35b4cf9098cd7fa91b6b0ad8ef9ab5fced2a65
Martin Jeanne <mjeanne@sio.local>
2021-04-01 09:37:45 +0200

    When login or logout now the page refresh, to actualize informations on it

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,16 +1,25 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import RSVP from 'rsvp';
 
 export default class IndexRoute extends Route {
   @service userAuth;
 
   model() {
-    return {};
+    if (this.userAuth.user) {
+      let employeeLogged = this.userAuth.user;
+      return RSVP.hash({
+        connected: employeeLogged,
+      });
+    }
+    else {
+      return {};
+    }
   }
 
   @action logout() {
     this.userAuth.logout();
-    this.transitionTo('index');
+    window.location.reload(true);
   }
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -17,7 +17,7 @@ export default class IndexRoute extends Route {
         let connected = employees.firstObject;
         if (connected.password && connected.password === user.password) {
           this.userAuth.login(connected);
-          this.transitionTo('dashboard');
+          window.location.reload(true);
         }
       }
     });
