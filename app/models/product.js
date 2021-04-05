1a2cfd28917b4fda6397faa50369e3302c74218a
Garwalle <Martin.Jeanne@sts-sio-caen.info>
2021-04-05 09:29:24 +0200

    The image of product are now showed !

--- app/models/product.js
+++ app/models/product.js
@@ -2,6 +2,7 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
 export default class ProductModel extends Model {
   @attr('string') name;
+  @attr('string') image;
   @belongsTo('section') section;
   @hasMany('product', { inverse: null }) packs;
 }
