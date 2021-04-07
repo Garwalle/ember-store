2b6853d21fd5b812465748a7e381e494b9d2698b
Garwalle <Martin.Jeanne@sts-sio-caen.info>
2021-04-07 23:03:42 +0200

    Doing some front end on the store.sections page on the products

--- app/models/product.js
+++ app/models/product.js
@@ -3,6 +3,7 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 export default class ProductModel extends Model {
   @attr('string') name;
   @attr('string') image;
+  @attr price;
   @belongsTo('section') section;
   @hasMany('product', { inverse: null }) packs;
 }
