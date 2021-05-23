b95a8cc15dc310197ab8e24f11c6c0f7bf4ae017
Garwalle <Martin.Jeanne@sts-sio-caen.info>
2021-05-23 17:00:06 +0200

    Changed host in app/adapters/application.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import RESTAdapter from '@ember-data/adapter/rest';
 
 export default class ApplicationAdapter extends RESTAdapter {
-  host = 'http://127.0.0.1:8090/rest';
+  host = 'http://solo-2.sts-sio-caen.info';
   urlForQuery(query, modelName) {
     if (query.sql) {
       query.filter = query.sql;
