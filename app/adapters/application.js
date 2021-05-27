7c591a57e8556a1c98593f7b2877f0b1d748fcd6
Garwalle <Martin.Jeanne@sts-sio-caen.info>
2021-05-27 10:27:49 +0200

    Changing host to deploy !

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,8 @@
 import RESTAdapter from '@ember-data/adapter/rest';
 
 export default class ApplicationAdapter extends RESTAdapter {
-  //host = 'http://solo-2.sts-sio-caen.info/rest';
-  host = 'http://127.0.0.1:8090/rest';
+  host = 'http://solo-2.sts-sio-caen.info/rest';
+  //host = 'http://127.0.0.1:8090/rest';
   urlForQuery(query, modelName) {
     if (query.sql) {
       query.filter = query.sql;
